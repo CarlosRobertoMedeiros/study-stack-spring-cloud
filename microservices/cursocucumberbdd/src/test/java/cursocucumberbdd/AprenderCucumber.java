de2f66a9b7c2ba69d72c7c15d18c748cd53803ef
Carlos Roberto <carlosmedeiroslima@gmail.com>
2023-06-19 18:20:12 -0300

    atualizando a app

--- microservices/cursocucumberbdd/src/test/java/cursocucumberbdd/AprenderCucumber.java
+++ microservices/cursocucumberbdd/src/test/java/cursocucumberbdd/AprenderCucumber.java
@@ -1,2 +1,23 @@
-package cursocucumberbdd;public class AprenderCucumber {
+package cursocucumberbdd;
+
+import io.cucumber.java.pt.Dado;
+import io.cucumber.java.pt.Então;
+import io.cucumber.java.pt.Quando;
+
+public class AprenderCucumber {
+    @Dado("que criei o arquivo corretamente")
+    public void queCrieiOArquivoCorretamente() {
+        // Write code here that turns the phrase above into concrete actions
+        //throw new io.cucumber.java.PendingException();
+    }
+    @Quando("executá-lo")
+    public void executáLo() {
+        // Write code here that turns the phrase above into concrete actions
+        System.out.println("Qualquer coisa");
+    }
+    @Então("a especificação deve finalizar com sucesso")
+    public void aEspecificaçãoDeveFinalizarComSucesso() {
+        // Write code here that turns the phrase above into concrete actions
+        //throw new io.cucumber.java.PendingException();
+    }
 }
